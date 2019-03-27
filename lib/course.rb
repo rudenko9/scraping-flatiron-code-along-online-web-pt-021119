be09a13549b72a779d8c55e55be4e86e8ecd5d83
Nataliya R <n.rudenko@gmail.com>
2019-03-27 20:15:10 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,7 +1,17 @@
 
 class Course
   attr_accessor :title, :schedule, :description
+  def initialize
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.reset_all
+    @@all.clear
+  end
   
-  d
 end 
 

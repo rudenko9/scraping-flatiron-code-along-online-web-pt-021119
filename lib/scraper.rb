f3262a235477d432bc38311cc6b70b8a65ada02b
Nataliya R <n.rudenko@gmail.com>
2019-03-27 20:56:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,9 +18,7 @@ class Scraper
   
 def get_page 
   doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  doc.css(".post").first.css("h2").text
-  doc.css(".post").first.css(".date").text
-  doc.css(".post").first.css("p").text
+  
   binding.pry
 end 
 end 
